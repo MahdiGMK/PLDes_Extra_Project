7eba56a454f7e17e27f1410749eeb6502c5e8790
MahdiGMK <mbahramiand@gmail.com>
2025-06-14 23:24:44 +0330

    typeinfo upd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+macro_rules! b {
+    ($x:expr) => {
+        Box::new($x)
+    };
+}
 #[derive(Debug, PartialEq)]
 enum VarType {
     Affine,
@@ -8,10 +13,23 @@ enum TypeInfo {
     Int,
     String,
     Boolean,
-    Record(Vec<TypeInfo>),
+    Record(Vec<(String, TypeInfo)>),
     List(Box<TypeInfo>),
 }
-fn main() {
-    let lof_lof_int = TypeInfo::List(Box::new(TypeInfo::List(Box::new(TypeInfo::Int))));
-    print!("{:?}", lof_lof_int);
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn type_system() {
+        let lof_lof_int = TypeInfo::List(b!(TypeInfo::List(b!(TypeInfo::Int))));
+        let lof_pii = TypeInfo::List(b!(TypeInfo::Record(vec![
+            ("x".into(), TypeInfo::Int),
+            ("y".into(), TypeInfo::Int),
+        ])));
+        println!("{:?}", lof_lof_int);
+        println!("{:?}", lof_pii);
+    }
 }
