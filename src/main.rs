41550eb016a17d1ca27d4409163d34a32ef9dcc4
MahdiGMK <mbahramiand@gmail.com>
2025-06-14 23:50:34 +0330

    complete structure

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,29 @@ macro_rules! b {
         Box::new($x)
     };
 }
+enum Value {
+    IntVal(i32),
+    BoolVal(bool),
+    StrVal(String),
+    RecVal(Vec<(String, Value)>),
+    ListVal(Vec<Value>),
+}
+enum Expr {
+    VarExpr(String),
+    ConstExpr(Value),
+    FuncEvalExpr(String, Vec<Expr>),
+    SumExpr(Box<Expr>, Box<Expr>),
+    NotExpr(Box<Expr>),
+    IfElseExpr(Box<Expr>, Box<Expr>, Box<Expr>),
+    FieldExpr(Box<Expr>, String),
+    IndexExpr(Box<Expr>, Box<Expr>),
+}
+enum STMT {
+    VarDef(VarType, String, Box<Expr>),
+    FuncDef(Vec<(VarType, String, TypeInfo)>, Box<Expr>),
+    RecDef(String, Vec<(String, TypeInfo)>),
+    Expr(Box<Expr>),
+}
 #[derive(Debug, PartialEq)]
 enum VarType {
     Affine,
@@ -16,6 +39,14 @@ enum TypeInfo {
     Record(Vec<(String, TypeInfo)>),
     List(Box<TypeInfo>),
 }
+struct Prog(Vec<STMT>);
+
+use TypeInfo::Boolean as BoolT;
+use TypeInfo::Int as IntT;
+use TypeInfo::List as ListT;
+use TypeInfo::Record as RecordT;
+use TypeInfo::String as StrT;
+
 fn main() {}
 
 #[cfg(test)]
@@ -24,11 +55,8 @@ mod tests {
 
     #[test]
     fn type_system() {
-        let lof_lof_int = TypeInfo::List(b!(TypeInfo::List(b!(TypeInfo::Int))));
-        let lof_pii = TypeInfo::List(b!(TypeInfo::Record(vec![
-            ("x".into(), TypeInfo::Int),
-            ("y".into(), TypeInfo::Int),
-        ])));
+        let lof_lof_int = ListT(b!(ListT(b!(IntT))));
+        let lof_pii = ListT(b!(RecordT(vec![("x".into(), IntT), ("y".into(), IntT),])));
         println!("{:?}", lof_lof_int);
         println!("{:?}", lof_pii);
     }
